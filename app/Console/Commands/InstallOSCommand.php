875ae116ce2be12b8394e2f5ff42d5aba4472f08
Mahdi Bagheri <mb.programmer@yahoo.com>
2025-07-31 18:50:58 +0330

    chore: move from constructor to handle

--- app/Console/Commands/InstallOSCommand.php
+++ app/Console/Commands/InstallOSCommand.php
@@ -12,36 +12,31 @@ class InstallOSCommand extends Command
 
     protected $description = 'Installing Packages and Dependencies';
 
-    public function __construct(
-        private readonly OperationSystemService $os,
-        private readonly WebServerService       $webServerService
-    )
-    {
-        parent::__construct();
-    }
-
     public function handle(): int
     {
+        $os = new OperationSystemService();
+        $webServerService = new WebServerService();
+
         $basePackages = config('devlet.dependencies.base_packages');
         $apacheModules = config('devlet.dependencies.apache_modules');
 
         $this->info("🔧 Installing packages: " . implode(', ', $basePackages));
 
-        $this->os->install($basePackages);
+       $os->install($basePackages);
 
         $this->info("✅ Packages was successfully installed.");
 
         $this->info("Web server modules enabling.");
 
-        $this->webServerService->enableModules($apacheModules);
+        $webServerService->enableModules($apacheModules);
 
         $this->info("✅ Web Server modules successfully enabled.");
 
-        if ($this->webServerService->isRunning()) {
-            $this->webServerService->reload();
+        if ($webServerService->isRunning()) {
+            $webServerService->reload();
             $this->info('✅ Web Server is successfully reloaded.');
         } else {
-            $this->webServerService->start();
+            $webServerService->start();
             $this->info('✅ Web Server is successfully started.');
         }
         $this->info("✅ Initialization completed successfully!");
